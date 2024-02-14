6ead21021ccf67faa409b9faa3f734f98d23e183
Ingvord <ingvord.mail@gmail.com>
2024-02-14 12:30:42 +0100

    show something

--- routes/api.js
+++ routes/api.js
@@ -13,10 +13,9 @@ router.get(`/users/:proposal_number`, (req, res) => {
     const proposal_number = req.params.proposal_number;
     const proposal = joint.find(proposal => proposal.proposal_number === proposal_number);
     if(!proposal) {
-        //TODO 403
+        return res.status(404).send({error:`Can not find proposal number: ${proposal_number}`})
     }
-    const proposalUsers = proposal.users.map(outer => users.find(user => user.id === outer.id));
-    res.json(proposalUsers);
+    res.json(proposal.users);
 });
 
 router.get('/proposals', (req, res) => {
--- test/mock/data.js
+++ test/mock/data.js
@@ -9,16 +9,18 @@ const experiments = [
 const users = [
     { id: 1, name: 'John Doe', orcid: '0000-0001-2345-6789' },
     { id: 2, name: 'Jane Smith', orcid: '0000-0002-3456-7890' },
+    { id: 3, name: 'Alice Smith', orcid: '0000-0003-4567-8901'},
+    { id: 4, name: 'Bob Johnson', orcid: '0000-0004-5678-9012'},
 ];
 
 // Mock data for demonstration
 const joint = [
     { id: 1, proposal_number: 'EXP-001', users: [
-            { id: 1 },
-            { id: 2 }
+            { id: 1, name: 'John Doe', orcid: '0000-0001-2345-6789' },
+            { id: 2, name: 'Jane Smith', orcid: '0000-0002-3456-7890' },
         ] },
     { id: 2, proposal_number: 'EXP-002', users: [
-            {id: 2 }
+            { id: 4, name: 'Bob Johnson', orcid: '0000-0004-5678-9012'}
         ] },
 ];
 
